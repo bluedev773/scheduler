7124410c3632886394c0e256a97c70a1599dd174
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-18 17:19:46 -0600

    create and test DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,10 +11,21 @@ export default function DayListItem(props) {
       "day-list__item--full": spots === 0
   });
 
+  const formatSpots = (spots) => {
+      if(spots === 0) {
+          return "no spots remaining";
+      }
+      if(spots === 1) {
+          return "1 spot remaining";
+      }
+
+      return `${spots} spots remaining`;
+  }
+
   return (
     <li onClick={() => setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
