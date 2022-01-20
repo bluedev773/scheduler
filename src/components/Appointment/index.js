070635ec4cc6b021b15505c1fbc86259f60c7871
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-19 21:31:00 -0600

    add static appointment data to application

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,8 @@ import Show from "./Show";
 import Empty from "./Empty";
 import "components/Appointment/styles.scss";
 
+
+
 export default function Appointment(props) {
     return(
         <article className="appointment">
