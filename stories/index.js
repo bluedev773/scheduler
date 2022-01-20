8d57e2e62e2092fb00b88d5d772cfe1abefe7e7a
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-19 20:43:54 -0600

    update Appointment Form to user props and state

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 //----------------------Button--------------------------------------
 
@@ -184,5 +185,21 @@ storiesOf("Appointment", module)
     <Error
        message="Could not delete appointment."
        onClose={action("onClose")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      student="Matthew Litvinov"
+      interviewer= {2}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
