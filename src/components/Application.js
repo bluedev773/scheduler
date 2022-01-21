241cdf94c4f6a67db04e024eecaa63530e8e3f54
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-21 15:41:35 -0600

    get days data from server instead of hardcoded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -65,6 +48,15 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const URL = `/api/days`;
+    axios.get(URL).then(response => {
+      setDays(response.data);
+    })
+  },[])
+
   const parsedAppointments = appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />)
   parsedAppointments.push(<Appointment key="last" time="5pm" />)
   return (
