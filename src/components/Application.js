eda7fff02da0820346fd5b33b1a9fad85873657d
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-01-23 13:38:12 -0600

    create and test getInterviewers per day selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -37,11 +37,13 @@ export default function Application(props) {
 
   const parsedAppointments = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
+    const interviewers = getInterviewersForDay(state, state.day);
     return(
       <Appointment
         key={appointment.id}
         {...appointment} 
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -20,22 +20,30 @@ export function getAppointmentsForDay(state, day) {
 
   }
 
-  export function getInterview(state, interview) {
+export function getInterview(state, interview) {
     if(interview === null) return null;
     const parsedInterview = interview;
     parsedInterview.interviewer = state.interviewers[interview.interviewer.toString()];
     return parsedInterview;
+}
+
+export function getInterviewersForDay(state, day) {
+
+    if(state.days.length === 0) {
+        return [];
+    }
+    const filteredByDay = state.days.filter(dayOfTheWeek => dayOfTheWeek.name === day);
+    if(filteredByDay.length === 0) {
+        return [];
+    }
+    const arrOfInterviewerIDs = filteredByDay[0].interviewers;
+    const arrOfInterviewers = [];
+    const interviewersAsArray = Object.values(state.interviewers);
+    arrOfInterviewerIDs.forEach(interviewerID => {
+        const filtered = interviewersAsArray.filter(interviewer => interviewer.id === interviewerID);
+        arrOfInterviewers.push(filtered[0]);
+    })
+
+    return arrOfInterviewers;
+
   }
-
-
-  //passed: interview
- // { student: "Archie Cohen", interviewer: 2 }
-//expected:
-//   {  
-//     "student": "Lydia Miller-Jones",
-//     "interviewer": {  
-//       "id": 1,
-//       "name": "Sylvia Palmer",
-//       "avatar": "https://i.imgur.com/LpaY82x.png"
-//     }
-//   }
